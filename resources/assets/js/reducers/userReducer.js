7dc51395e57901af72eca70c95a2ef7230a12a4e
Robert Burnham <burnhamrobertp@gmail.com>
2017-07-12 00:15:50 -0500

    Add generic user action and reducer

---
+++ resources/assets/js/reducers/userReducer.js
@@ -0,0 +1,60 @@
+import {handleActions} from 'redux-actions'
+
+const DEFAULT_STATE = {
+    id: null,
+    token: '',
+    display: '',
+    avatar: '',
+    email: '',
+    verified: 0,
+
+    isFetching: false,
+
+    loginRegisterModal: {
+        isOpen: false,
+        activeComponent: '',
+        email: '',
+        flashMessages: [],
+        flashMessageClass: ''
+    }
+};
+
+export default handleActions({
+    SET_LOGIN_REGISTER_MODAL_OPEN: (state, action) => ({
+        ...state,
+        loginRegisterModal: {
+            ...state.loginRegisterModal,
+            isOpen: action.payload
+        }
+    }),
+    SET_LOGIN_REGISTER_EMAIL: (state, action) => ({
+        ...state,
+        loginRegisterModal: {
+            ...state.loginRegisterModal,
+            email: action.payload
+        }
+    }),
+    SET_USER: (state, action) => action.payload,
+    SET_USER_FETCHING: (state, action) => ({
+        ...state,
+        isFetching: action.payload
+    }),
+    SET_ACTIVE_COMPONENT: (state, action) => ({
+        ...state,
+        loginRegisterModal: {
+            ...state.loginRegisterModal,
+            activeComponent: action.payload,
+            flashMessages: [],
+            flashMessageClass: ''
+        }
+    }),
+    SET_MESSAGES: (state, action) => ({
+        ...state,
+        loginRegisterModal: {
+            ...state.loginRegisterModal,
+            flashMessages: action.payload.messages,
+            flashMessageClass: action.payload.class
+        }
+    }),
+    GET_LOGOUT: (state, action) => DEFAULT_STATE,
+}, DEFAULT_STATE);
