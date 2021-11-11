5cdfe2cf78c3659abfc43a7ffe7960d6556bbf32
Robert Burnham <burnhamrobertp@gmail.com>
2021-11-11 12:28:56 -0600

    Clear out old laravel files

--- resources/assets/js/functions/stateHelpers.js
+++
@@ -1,9 +0,0 @@
-/**
- * User is both logged in and e-mail verified
- *
- * @param state
- * @returns {boolean}
- */
-export function userIsVerified(state) {
-    return typeof state.user.id === 'number' && state.user.verified === 1;
-}
